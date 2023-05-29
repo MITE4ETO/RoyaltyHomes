df81dcb3361c8b32666a676a0184da7a9d32115e
MITE4ETO <dimitrstefanov7v2@gmail.com>
2023-05-29 18:44:24 +0300

    added functionality with different modals  when clicked different buttons

--- modal.js
+++ modal.js
@@ -1,18 +1,46 @@
 "use strict";
 
-const modal = document.getElementById("modal");
-const modalCloseButton = document.getElementById("modal-close");
+const modalAbout = document.getElementById("modal-about");
+const modalRealEstateAgents = document.getElementById("modal-realEstateAgents");
+const modalCareers = document.getElementById("modal-careers");
+const modalSuggestions = document.getElementById("modal-suggestions");
+const modalCloseButtons = document.querySelectorAll(".modal-close");
 
 document.getElementById("about").addEventListener("click", function () {
-  modal.style.display = "block";
+  modalAbout.style.display = "block";
 });
 
+document.getElementById("realEstateAgents").addEventListener("click", function () {
+  modalRealEstateAgents.style.display = "block";
+});
 
+document.getElementById("careers").addEventListener("click", function () {
+  modalCareers.style.display = "block";
+});
+
+document.getElementById("suggestions").addEventListener("click", function () {
+  modalSuggestions.style.display = "block";
+});
+
+modalCloseButtons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    modalAbout.style.display = "none";
+    modalRealEstateAgents.style.display = "none";
+    modalCareers.style.display = "none";
+    modalSuggestions.style.display = "none";
+  });
+});
 
 window.addEventListener("click", function (event) {
-  if (event.target === modal) {
-    modal.style.display = "none";
+  if (
+    event.target === modalAbout ||
+    event.target === modalRealEstateAgents ||
+    event.target === modalCareers ||
+    event.target === modalSuggestions
+  ) {
+    modalAbout.style.display = "none";
+    modalRealEstateAgents.style.display = "none";
+    modalCareers.style.display = "none";
+    modalSuggestions.style.display = "none";
   }
 });
-
-
