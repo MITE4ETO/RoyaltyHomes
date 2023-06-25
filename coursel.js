7dfc0d9114cf9a47c709720b243335e04ed03284
MITE4ETO <dimitrstefanov7v2@gmail.com>
2023-06-25 14:42:01 +0300

    fixed bugs

--- coursel.js
+++ coursel.js
@@ -1,10 +1,43 @@
-let slideIndex = 0; // Initialize slide index to 0
+let slideIndex1 = 0; // Initialize slide index to 0
+let slideIndex2 = 0; // Initialize slide index to 0
+let slideIndex3 = 0; // Initialize slide index to 0
+let slideIndex4 = 0; // Initialize slide index to 0
 
 showSlides(); // Call the showSlides function to display the initial slide
 
 // Next/previous controls
-function plusSlides(n) {
-  slideIndex += n; // Increment the slide index by n
+function plusSlides(index, n) {
+  const slidesLength = 3;
+
+  if (index === 0) {
+    slideIndex1 += n;
+    if (slideIndex1 === 3) {
+      slideIndex1 = 0;
+    } else if (slideIndex1 === -1) {
+      slideIndex1 = 2;
+    }
+  } else if (index === 1) {
+    slideIndex2 += n;
+    if (slideIndex2 === 3) {
+      slideIndex2 = 0;
+    } else if (slideIndex2 === -1) {
+      slideIndex2 = 2;
+    }
+  } else if (index === 2) {
+    slideIndex3 += n;
+    if (slideIndex3 === 3) {
+      slideIndex3 = 0;
+    } else if (slideIndex3 === -1) {
+      slideIndex3 = 2;
+    }
+  } else if (index === 3) {
+    slideIndex4 += n;
+    if (slideIndex4 === 3) {
+      slideIndex4 = 0;
+    } else if (slideIndex4 === -1) {
+      slideIndex4 = 2;
+    }
+  }
   showSlides();
 }
 
@@ -16,25 +49,33 @@ function currentSlide(n) {
 
 function showSlides() {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+  let slides1 = document.getElementsByClassName('mySlides1');
+  let slides2 = document.getElementsByClassName('mySlides2');
+  let slides3 = document.getElementsByClassName('mySlides3');
+  let slides4 = document.getElementsByClassName('mySlides4');
+
+  let dots1 = document.getElementsByClassName('dots1');
+  let dots2 = document.getElementsByClassName('dots2');
+  let dots3 = document.getElementsByClassName('dots3');
+  let dots4 = document.getElementsByClassName('dots4');
 
   // Reset slide index if it goes beyond the number of slides
-  if (slideIndex >= slides.length) {
-    slideIndex = 0;
-  } else if (slideIndex < 0) {
-    slideIndex = slides.length - 1;
-  }
+  [
+    { slides: slides1, dots: dots1, index: slideIndex1 },
+    { slides: slides2, dots: dots2, index: slideIndex2 },
+    { slides: slides3, dots: dots3, index: slideIndex3 },
+    { slides: slides4, dots: dots4, index: slideIndex4 },
+  ].forEach(({ slides, dots, index }) => {
+    // Hide all slides and remove active class from dots
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none';
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(' active', '');
+    }
 
-  // Hide all slides and remove active class from dots
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-
-  // Display the current slide and set active class on corresponding dot
-  slides[slideIndex].style.display = "block";
-  dots[slideIndex].className += " active";
+    // Display the current slide and set active class on corresponding dot
+    slides[index].style.display = 'block';
+    dots[index].className += ' active';
+  });
 }
--- favButtonClick.js
+++ favButtonClick.js
@@ -2,10 +2,20 @@
 
 const favButtons = document.getElementsByClassName("buttonFav");
 const navFavorite = document.getElementById("favorites");
-let counter = 0;
+let favorites = [];
 
 for (let i = 0; i < favButtons.length; i++) {
   favButtons[i].onclick = function () {
-    navFavorite.setAttribute("value", ++counter);
+    const exists = favorites.indexOf(i);
+
+    if (exists === -1) {
+      favorites.push(i);
+      favButtons[i].textContent = 'Remove from favourites';
+    } else {
+      favorites.splice(exists, 1);
+      favButtons[i].textContent = 'Add to favourites';
+    }
+
+    navFavorite.setAttribute("value", favorites.length);
   };
 }
